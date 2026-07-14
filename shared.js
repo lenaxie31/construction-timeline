15b18cd07ea953e4b8a0df719e575b677764ce54
lenaxie31 <lenaxie31@gmail.com>
2026-07-14 09:44:53 +0800

    Add files via upload

--- shared.js
+++ shared.js
@@ -484,7 +484,8 @@ function buildHeader(pageTitle) {
   return `
   <div class="header">
     <button class="hamburger-btn" id="hamburger-btn" onclick="toggleNavMenu()" title="選單" aria-label="選單">
-      <span></span><span></span><span></span>
+      <span class="hamburger-icon"><span></span><span></span><span></span></span>
+      <span class="hamburger-label">MENU</span>
     </button>
     <div class="header-logo">
       <svg viewBox="0 0 24 24"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
